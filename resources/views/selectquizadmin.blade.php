a59cb8ff916320db3ea887af99c22e261015a999
Neo_Tsang <neo@tsang.nu>
2025-07-01 11:29:40 +0200

    quiz selectie toegevoegd

---
+++ resources/views/selectquizadmin.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            color: #333;
+            text-align: center;
+            padding: 50px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: space-around;
+            height: 40vh;
+        }
+        div {
+            margin: 20px;
+            padding: 10px;
+            background-color: #ecf0f1;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0,0,0,0.1);
+            width: 50vw;
+            justify-content: space-between;
+            display: flex;
+            flex-direction: row;
+        }
+        h1 {
+            margin-top:10vh;
+            color: #2c3e50;
+            font-size: 40px;
+        }
+        button {
+            padding: 15px 20px;
+            background-color: #2980b9;
+            color: white;
+            border: none;
+            cursor: pointer;
+            border-radius: 5px;
+            font-size: 16px;
+        }
+        button:hover {
+            background-color: #3498db;
+        }
+    </style>
+</head>
+<body>
+    <h1>Welcome Admin</h1>
+    <div>
+        <h2>Quiz 1</h2>
+        <button type="submit">Edit</button>
+    </div>
+    <div>
+        <h2>Quiz 2</h2>
+        <button type="submit">Edit</button>
+    </div>
+    <div>
+        <h2>Quiz 3</h2>
+        <button type="submit">Edit</button>
+    </div>
+</body>
+</html>
