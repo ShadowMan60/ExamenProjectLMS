46ddc965c5ded633eedf4c94805573623feb6be2
Neo_Tsang <neo@tsang.nu>
2025-07-01 14:30:44 +0200

    result, edit en question toegevoegd

---
+++ resources/views/question.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            color: #333;
+            text-align: center;
+            padding: 50px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: space-around;
+            height: 40vh;
+        }
+        h1 {
+            margin-top:10vh;
+            color: #2c3e50;
+            font-size: 40px;
+        }
+        button {
+            margin-top: 25px;
+            margin-bottom: 10px;
+            padding: 15px 20px;
+            background-color: #2980b9;
+            color: white;
+            border: none;
+            cursor: pointer;
+            border-radius: 5px;
+            font-size: 16px;
+        }
+        button:hover {
+            background-color: #3498db;
+        }
+    </style>
+</head>
+<body>
+    <h1>Question 1 lorem ipsum</h1>
+    <button>A lorem ipsum</button>
+    <button>B lorem ipsum</button>
+    <button>C lorem ipsum</button>
+</body>
+</html>
---
+++ resources/views/result.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            color: #333;
+            text-align: center;
+            padding: 50px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: space-around;
+            height: 40vh;
+        }
+        div {
+            margin: 20px;
+            padding: 10px;
+            background-color:rgb(32, 159, 190);
+            border-radius: 10vh;
+            box-shadow: 0 2px 5px rgba(0,0,0,0.1);
+            width: 70vw;
+            height: 20vh;
+            justify-content: center;
+            align-items: center;
+            display: flex;
+            flex-direction: row;
+        }
+        p{
+            font-size: 30px;
+            color: white;
+            font-weight: bold;
+        }
+        h1 {
+            margin-top:10vh;
+            color: #2c3e50;
+            font-size: 40px;
+        }
+        button {
+            padding: 15px 20px;
+            background-color: #2980b9;
+            color: white;
+            border: none;
+            cursor: pointer;
+            border-radius: 5px;
+            font-size: 16px;
+        }
+        button:hover {
+            background-color: #3498db;
+        }
+    </style>
+</head>
+<body>
+    <div><p>??%</p></div>
+    <h1>??/?? Correct</h1>
+    <button>return</button>
+</body>
+</html>
